b1e6a66861571a67f05215d2c86d3f594c641de2
jiyoulee <jiyouleehome@gmail.com>
2021-01-05 11:33:18 +0900

    Add script for en/disabling hyperthreading on server4

---
+++ hyperthreading.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Script for en/disabling hyperthreading at runtime.
+# Before executing script, log in as root.
+
+# Enable.
+# echo on > /sys/devices/system/cpu/smt/control
+
+# Disable.
+echo off > /sys/devices/system/cpu/smt/control
+
+# Verify (IDLE).
+mpstat -A 1 1
