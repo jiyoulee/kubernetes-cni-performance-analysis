59e510c104f6f010c94a2ba82f24011aa8706419
jiyoulee <jiyouleehome@gmail.com>
2021-01-20 08:29:08 +0900

    Delete Overlay Host, Overlay Old

--- overlay-host/net-create.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-NAME=ov-n-memcached
-
-# Create bridge network
-docker network create -d overlay --attachable $NAME
-
-# Verify bridge network
-docker network inspect $NAME
--- overlay-old/stop.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-docker kill $(docker ps -aq)
-docker rm $(docker ps -aq)
