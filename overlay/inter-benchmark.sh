f19228c0138fe49f4e04bec6f1cfe9ded128ee62
jiyoulee <jiyouleehome@gmail.com>
2021-01-20 13:45:06 +0900

    [Overlay/Inter/Pri] Create 5th experiment Memaslap scripts

--- overlay/inter-benchmark.sh
+++ overlay/inter-benchmark.sh
@@ -2,14 +2,16 @@
 
 CONCURRENCY=512
 FILE=./results/benchmark.txt
-IPADDR1=172.32.0.2
-IPADDR2=172.32.0.3
-IPADDR3=172.34.0.2
-IPADDR4=172.34.0.3
+IPADDR1=10.0.2.84
+IPADDR2=10.0.2.86
+# IPADDR3=172.34.0.2
+# IPADDR4=172.34.0.3
 PORT=11211
 THREADS=16
 
-./../../memcached_bench/libmemcached-1.0.15/clients/memaslap -B -c ${CONCURRENCY} -s ${IPADDR1}:${PORT},${IPADDR2}:${PORT},${IPADDR3}:${PORT},${IPADDR4}:${PORT} -T ${THREADS} | dd status=none of=$FILE conv=notrunc oflag=append
+docker exec m1 /memcached_bench/libmemcached-1.0.15/clients/memaslap -B -c ${CONCURRENCY} -s ${IPADDR1}:${PORT},${IPADDR2}:${PORT} -T ${THREADS} -t 70s | dd status=none of=$FILE conv=notrunc oflag=append
+
+# ./../../memcached_bench/libmemcached-1.0.15/clients/memaslap -B -c ${CONCURRENCY} -s ${IPADDR1}:${PORT},${IPADDR2}:${PORT},${IPADDR3}:${PORT},${IPADDR4}:${PORT} -T ${THREADS} | dd status=none of=$FILE conv=notrunc oflag=append
 
 echo "BENCHMARK done..."
 
