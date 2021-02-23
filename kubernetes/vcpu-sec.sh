1a2d129134dd335404caf33d8f46ea21f95ae8ce
jiyoulee <jiyouleehome@gmail.com>
2021-02-23 09:22:07 +0900

    [k8s/Flannel] Modify: make cpu-sec.sh variables work

--- kubernetes/vcpu-sec.sh
+++ kubernetes/vcpu-sec.sh
@@ -5,8 +5,8 @@ MEMC2_ID=2b4650d1b99d
 
 for (( i=1; i<=60; i++));
 do
-	docker stats --no-stream --format "{{.CPUPerc}}" MEMC1_ID >> ./results/vcpu_memc1.txt &
-	docker stats --no-stream --format "{{.CPUPerc}}" MEMC2_ID >> ./results/vcpu_memc2.txt &
+	docker stats --no-stream --format "{{.CPUPerc}}" $MEMC1_ID >> ./results/vcpu_memc1.txt &
+	docker stats --no-stream --format "{{.CPUPerc}}" $MEMC2_ID >> ./results/vcpu_memc2.txt &
 	sleep 1s;
 done
 
